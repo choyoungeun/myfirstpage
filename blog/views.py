0941131da2ecae4c81755e1d242dd354fd830ffb
JoYeongEun <duddmd0219@naver.com>
2019-03-03 23:47:37 +0900

    subtle change

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Blog
+from django.core.paginator import Paginator
 from django.utils import timezone
 
 def home(request):
     blogs = Blog.objects
-    return render(request,'home.html',{'blogs':blogs})
+    blog_list = Blog.objects.all().order_by('-id')
+    #모든 글들을 대상으로(객체는 최신순으로 정렬)
+    paginator = Paginator(blog_list,3)
+    #블로그 객체 3개를 한 페이지로 자르기
+    page = request.GET.get('page')
+    #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담는다)
+    posts = paginator.get_page(page)
+    #request된 페이지를 얻어온 뒤 return해 준다.
+    return render(request,'home.html',{'blogs':blogs, 'posts':posts})
 
 def detail(request,blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
--- secondproject/urls.py
+++ secondproject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,4 +15,7 @@ urlpatterns = [
     path('blog/create/',blog.views.create,name='create'),
     path('blog/contact/',blog.views.contact,name='contact'),
     path('portfolio/',portfolio.views.portfolio,name='portfolio'),
+    path('accounts/signup/',accounts.views.signup,name='signup'),
+    path('accounts/login/',accounts.views.login,name='login'),
+    path('accounts/logout/',accounts.views.logout,name='logout'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
