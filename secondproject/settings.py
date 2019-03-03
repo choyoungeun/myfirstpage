0bbb90169c7fef41ece0ba2e369678269fa5912e
JoYeongEun <duddmd0219@naver.com>
2019-03-04 00:52:23 +0900

    subtle change

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'f7#-s!!%&9w$eshmg^vx)q#liwc$je
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
